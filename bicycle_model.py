a6f2c5f4e908947152548458043cbed61efb277e
nicoo <2926155258@qq.com>
2024-12-11 22:15:53 +0800

    自己写的单车模型

---
+++ bicycle_model.py
@@ -0,0 +1,24 @@
+import math
+
+
+class BicycleModel:
+    def __init__(self, x, y, psi, v, f_len, r_len):
+        self.x = x
+        self.y = y
+        self.psi = psi
+        self.v = v
+
+        self.f_len = f_len
+        self.r_len = r_len
+
+    def get_state(self):
+        return self.x, self.y, self.psi, self.v
+
+    def update_state(self, a, delta_f, delta_r, dt):
+        beta = math.atan((math.tan(delta_f) * self.r_len + math.tan(delta_r) * self.f_len)/(self.f_len + self.r_len))
+
+        self.x = self.x + self.v * math.cos(self.psi + beta) * dt
+        self.y = self.y + self.v * math.sin(self.psi + beta) * dt
+        self.psi = self.v * math.cos(beta) / (self.f_len + self.r_len) * (math.tan(delta_f) - math.tan(delta_r)) * dt
+        self.v = self.v + a * dt
+
